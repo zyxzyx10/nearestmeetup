2f682fd7df017f54a6149cbf78793a7f23789e45
Paul Zhang <paul.zhang@central1.com>
2019-11-01 11:39:40 -0700

    code refine

--- src/main/java/com/paul/zhang/NearestMeetup.java
+++ src/main/java/com/paul/zhang/NearestMeetup.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 public class NearestMeetup {
 	public static void main(String[] args) throws URISyntaxException, IOException {
 		if (args == null || args.length <= 0) {
-			System.out.println("please provide an adrees at least");
+			System.out.println("please provide an address at least");
 			System.exit(-1);
 		}
 
